a9d464b8391c893490241bd3962ac18688c8e3c5
marshallhelenm <marshallhelenm@gmail.com>
2019-09-04 09:07:20 -0700

    done

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-// It might be a good idea to add event listener to make sure this file 
-// only runs after the DOM has finshed loading. 
 document.addEventListener("DOMContentLoaded", main)
+
+const baseURL = 'http://localhost:3000'
+const embedLink = "http://localhost:3000/quotes?_embed=likes"
+
 
 function main(event){
     event.preventDefault()
     listQuotes()
     newQuote()
 }
-
-const embedLink = "http://localhost:3000/quotes?_embed=likes"
-const baseURL = 'http://localhost:3000'
 
 function listQuotes(){
     let ul = document.getElementById('quote-list')
@@ -32,69 +31,144 @@ function createQuoteCard(quote){
     li.setAttribute('class', 'quote-card')
     li.setAttribute("id", quote.id)
     let block = document.createElement("blockquote")
-    li.appendChild(block)
     
-
     let p = document.createElement("p")
     p.setAttribute("class", "mb-0")
+    p.setAttribute("id", `quote-${quote.id}`)
     p.textContent = quote.quote
-    block.appendChild(p)
-
+    
     let footer = document.createElement("footer")
     footer.setAttribute("class", "blockquote-footer")
+    footer.setAttribute("id", `auth-${quote.id}`)
+
     footer.textContent = quote.author
-    block.appendChild(footer)
-    block.appendChild(document.createElement("br"))
-
-    createLikeBtn(quote, block)
-
-
+    
+    let likeBtn = createLikeBtn(quote)
+    
     let btnDanger = document.createElement("button")
     btnDanger.setAttribute('class', 'btn-danger')
     btnDanger.setAttribute('id', quote.id)
     btnDanger.textContent = "Delete"
     btnDanger.addEventListener("click", deleteQuote)
+    
+    let editBtn = document.createElement("button")
+    editBtn.setAttribute('id', quote.id)
+    editBtn.textContent = "Edit"
+    editBtn.addEventListener("click", hideForm)
+    
+    let editForm = createEditForm(quote)
+
+    let editDiv = document.createElement('div')
+    editDiv.appendChild(editForm)
+    editDiv.appendChild(editBtn)
+
+    
+    li.appendChild(block)
+    block.appendChild(p)
+    block.appendChild(footer)
+    block.appendChild(likeBtn)
+    block.appendChild(document.createElement("br"))
     block.appendChild(btnDanger)
-
-
+    block.appendChild(editDiv)
     return li
     
 }
 
-function createLikeBtn(quote, block){
+function createLikeBtn(quote){
+    let span = document.createElement("span")
+    span.textContent = quote.likes.length
+    
     let likeBtn = document.createElement("button")
     likeBtn.setAttribute('class', 'btn-success')
     likeBtn.setAttribute("id", quote.id)
     likeBtn.textContent = "Likes: "
-    likeBtn.addEventListener("click", likeQuote)
-    
-    let span = document.createElement("span")
-    span.setAttribute("id", `span${quote.id}`)
+    likeBtn.onclick = e => {
+        quote.likes.push(1);
+        span.textContent = quote.likes.length
+        createLike(quote)
+    }
     
     likeBtn.appendChild(span)
-    block.appendChild(likeBtn)
-    listLikes(quote)
-}
-
-
-function listLikes(quote){
-    let spanId = `span${quote.id}`
-    console.log(quote.id)
-    let span = document.getElementById('span'+quote.id)
+    return likeBtn
     
-    fetch("http://localhost:3000/likes")
-        .then(response => response.json())
-        .then(likes => {
-            let myLikes = 0
-            for (i in likes){
-                if (likes[i].quoteId == quote.id) {
-                    myLikes++
-                }
-            }
-            span.textContent = myLikes
-        })
 }
 
+function createEditForm(quote){
+    let form = document.createElement("form")
+
+    let quoteDiv = document.createElement("div")
+    let quoteLabel = document.createElement("label")
+    quoteLabel.setAttribute('for', 'edit-quote')
+
+    let quoteField = document.createElement('input')
+    quoteField.setAttribute('type', 'text-area')
+    quoteField.classList.add('form-control')
+    quoteField.setAttribute('for', 'edit-quote')
+    quoteField.setAttribute('placeholder', quote.quote)
+    quoteField.setAttribute('id', `edit-quote-${quote.id}`)
+
+    quoteDiv.appendChild(quoteLabel)
+    quoteDiv.appendChild(quoteField)
+
+
+
+    let authDiv = document.createElement("div")
+    let authLabel = document.createElement("label")
+    authLabel.setAttribute('for', 'edit-auth')
+
+    let authField = document.createElement('input')
+    authField.setAttribute('type', 'text')
+    authField.classList.add('form-control')
+    authField.setAttribute('for', 'edit-auth')
+    authField.setAttribute('placeholder', quote.author)
+    authField.setAttribute('id', `edit-auth-${quote.id}`)
+
+    authDiv.appendChild(authLabel)
+    authDiv.appendChild(authField)
+    
+    let submitBtn = document.createElement("button")
+    submitBtn.classList.add('btn', 'btn-primary')
+    submitBtn.setAttribute('type', 'submit')
+    submitBtn.textContent = 'Submit Change'
+    submitBtn.addEventListener('click', event => {
+        hideForm(event)
+
+        let newText = event.target.parentNode.children[0].children[1].value
+        console.log(newText)
+        quote.quote = newText
+        let p = document.getElementById(`quote-${quote.id}`)
+        p.textContent = newText
+
+        let newAuth = event.target.parentNode.children[1].children[1].value
+        console.log(newAuth)
+        quote.author = newAuth
+        let footer = document.getElementById(`auth-${quote.id}`)
+        footer.textContent = newAuth
+    
+        
+        fetch(`${baseURL}/quotes/${quote.id}`, {
+            method: "PATCH",
+            headers: {
+                'Content-Type':'application/json',
+                Accept: "application/json"
+              },
+              body: JSON.stringify({
+                  'quote': newText,
+                  'author': newAuth
+              })
+        })
+
+
+    })
+ 
+    form.appendChild(quoteDiv)
+    form.appendChild(authDiv)
+    form.appendChild(submitBtn)
+    form.style.display="none"
+
+    return form
+
+}
 
 function deleteQuote(event){
     fetch(`http://localhost:3000/quotes/${event.target.id}`, {
@@ -135,13 +209,6 @@ function newQuote(){
     })
 }
 
-function likeQuote(event){
-    event.preventDefault()
-    fetch(`${baseURL}/quotes/${event.target.id}`)
-        .then(response => response.json())
-        .then(quote => createLike(quote))
-}
-
 function createLike(quote){
     fetch(`${baseURL}/likes`, {
         method: 'POST',
@@ -149,9 +216,44 @@ function createLike(quote){
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-            quoteId: quote.id,
+            quoteId: parseInt(quote.id),
             createdAt: Date.now()
         })
     })
-    listLikes(quote)
 }
+
+
+function hideForm(event) {
+    event.preventDefault
+    let editForm = event.target.parentNode.firstChild
+    if (event.target.textContent == "Hide Form") {
+        event.target.textContent = "Edit"
+      editForm.style.display = 'none'
+    } else if (event.target.textContent == "Edit") {
+        console.log('hit')
+        event.target.textContent = "Hide Form"
+      editForm.style.display = 'block'
+    }
+
+} 
+
+const sortBtn = document.getElementById('sort-btn')
+sortBtn.addEventListener('click', event => {
+    let ul = document.getElementById('quote-list')
+    while (ul.firstChild) {
+        ul.removeChild(ul.firstChild)
+    }
+    fetch(embedLink)
+        .then(response => response.json())
+        .then(quotes => {
+            sortedQuotes = quotes.sort((a, b) => {
+                return a.author.localeCompare(b.author)
+            })
+            console.log(sortedQuotes)
+            sortedQuotes.forEach(quote => {
+                let card = createQuoteCard(quote)
+                ul.appendChild(card)
+            })
+        })
+})
+
